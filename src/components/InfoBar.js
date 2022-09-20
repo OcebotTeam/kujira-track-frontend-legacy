ff2bfd606347e1258630cda486739baac8b53917
Pedro Delgado <pdelgado@ocebot.com>
2022-09-20 22:54:36 +0200

    Popper lib added.

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -67,7 +67,7 @@ const InfoBar = (props) => {
       <div className="row">
         <div className="col text-white fs-5 fw-bold px-4 text-lg-center text-xxl-start">
           <span className="d-inline d-lg-block d-xxl-inline"><i className="bi bi-list-ul align-text-top me-3"></i>Total transactions</span>
-          <div className="float-end float-lg-none float-xxl-end fw-light"> {transactions} </div>
+          <div className="float-end float-lg-none float-xxl-end fw-light text-dark"> {transactions} </div>
         </div>
 
         <div className="vr p-0 text-white d-none d-lg-block"></div>
@@ -75,7 +75,7 @@ const InfoBar = (props) => {
 
         <div className="col text-white fs-5 fw-bold px-4 text-lg-center text-xxl-start">
           <span className="d-inline d-lg-block d-xxl-inline"><i className="bi bi-graph-up-arrow align-text-top me-3"></i>Kujira/axlUSDC</span>
-          <div className="float-end float-lg-none float-xxl-end fw-light"> ${price} </div>
+          <div className="float-end float-lg-none float-xxl-end fw-light text-dark"> ${price} </div>
         </div>
 
         <div className="vr p-0 text-white d-none d-lg-block"></div>
@@ -83,7 +83,7 @@ const InfoBar = (props) => {
 
         <div className="col text-white fs-5 fw-bold px-4 text-lg-center text-xxl-start">
           <span className="d-inline d-lg-block d-xxl-inline"><i className="bi bi-lock align-text-top me-3"></i>Total staked</span>
-          <div className="float-end float-lg-none float-xxl-end fw-light"> {stake} </div>
+          <div className="float-end float-lg-none float-xxl-end fw-light text-dark"> {stake} </div>
         </div>
       </div>
     </div>
