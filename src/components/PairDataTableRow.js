bb447fe06f133f2e4140210e65a5f8f88cf4da88
Pedro Delgado <pdelgado@ocebot.com>
2022-09-20 12:51:13 +0200

    Corrections to price and volume calculations.

--- src/components/PairDataTableRow.js
+++ src/components/PairDataTableRow.js
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 import Pair from "../data/Pair";
+import { numberWithCommas } from "../data/Math";
 
 const PairDataTableRow = (props) => {
   const { ticker_id } = props;
 
   const [volume, setVolume] = useState("...");
+  const [price, setPrice] = useState("...");
 
   useEffect(() => {
     const pair = new Pair(ticker_id);
     const volume = pair.lastDayVolume();
-    volume.then(value => setVolume(value));
+    const price = pair.currentPrice();
+
+    volume.then((value) => setVolume(numberWithCommas(value.toFixed(0))));
+    price.then((value) => setPrice(value.toFixed(3)));
   }, [ticker_id]);
 
   return (
     <tr>
       <td className="text-muted">{ticker_id}</td>
-      <td className="text-center">$000</td>
+      <td className="text-center">${price}</td>
       <td className="text-end">${volume}</td>
     </tr>
   );
--- src/data/Pair.js
+++ src/data/Pair.js
@@ -1,4 +1,4 @@
-import pairs from "./pairs.json";
+import Pairs from "./pairs.json";
 import TickPrecision from "./TickPrecision";
 
 /**
@@ -19,32 +19,35 @@ const dateToApiFormat = (date, tzString = "UTC") => {
 };
 
 /**
+ * Returns proper volume amount.
+ *
+ * @param ticker_id {TickPrecision}
+ * @param rawVolume
+ * @returns {Promise<Number>}
+ */
+const adjustedVolume = (ticker_id, rawVolume) => {
+  const divider = 1000000;
+  const nominativeTickerId = Pairs[ticker_id].nominative;
+
+  if (nominativeTickerId !== undefined) {
+    const nominativePair = new Pair(nominativeTickerId);
+    return nominativePair
+      .lastDayPrice()
+      .then((price) => (rawVolume / divider) * price);
+  } else {
+    return new Promise((resolve) => {
+      resolve(rawVolume / divider);
+    });
+  }
+};
+
+/**
  * Pair object
  *
  * @param ticker_id
  * @constructor
  */
 function Pair(ticker_id) {
-  /**
-   * Returns the current pair market price
-   * @returns {Promise<any>}
-   */
-  this.currentPrice = () => {
-    const endpoint =
-      process.env.REACT_APP_API_BRIDGE_URL +
-      "/kbridge/trades?contract=" +
-      pairs[ticker_id].contract;
-
-    return fetch(endpoint)
-      .then((response) => response.json())
-      .then(
-        (json) =>
-          json.trades.filter((trade) => trade.type === "buy").shift()
-            .trade_price
-      )
-      .catch((error) => console.log(error));
-  };
-
   /**
    * Returns candle raw values from de API given a precision period
    * @param precision {TickPrecision}
@@ -54,12 +57,12 @@ function Pair(ticker_id) {
   this.candlesRawValues = (precision, periods) => {
     const toDate = new Date();
     const fromDate = new Date();
-    fromDate.setDate(fromDate.getDate() - periods);
+    fromDate.setDate(fromDate.getDate() - periods + 1);
 
     let endpoint =
       process.env.REACT_APP_API_BRIDGE_URL +
       "/kbridge/candles?contract=" +
-      pairs[ticker_id].contract +
+      Pairs[ticker_id].contract +
       "&precision=" +
       precision +
       "&from=" +
@@ -73,12 +76,60 @@ function Pair(ticker_id) {
       .catch((error) => console.log(error));
   };
 
-  this.lastDayVolume = () => {
-    return this.candlesRawValues(TickPrecision.day1, 1).then((candles) => {
-      return candles.pop().volume;
+  /**
+   * Returns the current pair market price
+   * @returns {Promise<Number>}
+   */
+  this.currentPrice = () => {
+    return this.candlesRawValues(TickPrecision.day1, 30).then((candles) => {
+      for (let i = candles.length - 1; i >= 0; i--) {
+        if (candles[i].close !== null) {
+          let price = candles[i].close;
+
+          if (Pairs[ticker_id].priceMultiplier !== undefined) {
+            price *= Pairs[ticker_id].priceMultiplier;
+          }
+
+          return Number(price);
+        }
+      }
     });
   };
 
+  /**
+   * Returns last day pair market price
+   * @returns {Promise<Number>}
+   */
+  this.lastDayPrice = () => {
+    return this.candlesRawValues(TickPrecision.day1, 30).then((candles) => {
+      for (let i = candles.length - 2; i >= 0; i--) {
+        if (candles[i].close !== null) {
+          let price = candles[i].close;
+
+          if (Pairs[ticker_id].priceMultiplier !== undefined) {
+            price *= Pairs[ticker_id].priceMultiplier;
+          }
+
+          return Number(price);
+        }
+      }
+    });
+  };
+
+  /**
+   * Returns last day total volume
+   * @returns {Promise<Number>}
+   */
+  this.lastDayVolume = () => {
+    return this.candlesRawValues(TickPrecision.day1, 2).then((candles) => {
+      return adjustedVolume(ticker_id, candles[candles.length - 2].volume);
+    });
+  };
+
+  /**
+   * Returns candles data adapted to Lightweigth Charts library.
+   * @returns {Promise<Array>}
+   */
   this.candlesChartValues = (precision, periods) => {
     return this.candlesRawValues(precision, periods).then((candles) => {
       return candles.map((candle) => {
