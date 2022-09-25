599e5c5a11ebf83ca1a65889303d96d929dbcea5
Pedro Delgado <pdelgado@ocebot.com>
2022-09-25 23:37:36 +0200

    Volumes info-bar responsiveness update.

--- src/components/volumes/VolumesInfoBar.js
+++ src/components/volumes/VolumesInfoBar.js
@@ -17,17 +17,21 @@ const VolumesInfoBar = () => {
   }, []);
 
   return (
-    <div className="blue-gradient-bg rounded p-4 container-fluid">
+    <div className="blue-gradient-bg rounded p-4 container-fluid fs">
       <div className="row">
-        <div className="col text-white fs-5 fw-bold px-4 text-lg-center text-xxl-start">
-          <span className="d-inline d-lg-block d-xxl-inline">
+        <div className="col text-white fs-5 fw-bold px-4 text-center text-md-start">
+
+          <span className="d-block d-md-inline">
             <i className="bi bi-bar-chart-fill align-text-top me-3"></i>
-            Accumulated volume{" "}
-            <span className="text-dark fw-light">/ yesterday</span>
+            Accumulated volume
           </span>
-          <div className="float-end float-lg-none float-xxl-end fw-light text-dark">
+          <span className="text-dark fw-light d-none d-md-inline mx-2">/</span>
+          <span className="text-dark fw-light">yesterday</span>
+
+          <div className="float-md-end fw-light text-dark">
             ${lastDayVol}
           </div>
+
         </div>
       </div>
     </div>
