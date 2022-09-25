c0425dfcb53d8516049a303126505593286fc87d
Pedro Delgado <pdelgado@ocebot.com>
2022-09-25 15:26:45 +0200

    Volumes filtering.

--- src/routes/Volumes.js
+++ src/routes/Volumes.js
@@ -3,28 +3,43 @@ import pairs from "../data/pairs.json";
 import tickPrecision from "../data/TickPrecision";
 import FinVolumeChart from "../charts/FinVolumeChart";
 import VolumesInfoBar from "../components/volumes/VolumesInfoBar";
+import { useEffect, useState } from "react";
 
 const Volumes = () => {
+  const [filter, setFilter] = useState("");
+  const [charts, setCharts] = useState([]);
 
-  const volumeCharts = () => {
+  const filteredCharts = () => {
+    return charts.filter(chart => {
+      return !!(chart.key.toLowerCase().includes(filter));
+    })
+  }
+
+  filteredCharts();
+
+  useEffect(() => {
     const charts = [];
-
     for (const pairId in pairs) {
       charts.push(
         <div key={"volume" + pairId} className="col-lg-6 mb-3 mb-lg-4">
           <div className="card bg-dark">
             <div className="card-body">
               <h5 className="text-muted fw-light">Volume (1D)</h5>
-              <h3 className="card-title text-white">{String(pairId).replace("_", "/")}</h3>
-              <FinVolumeChart tickerId={pairId} precision={tickPrecision.day1} period={100} />
+              <h3 className="card-title text-white">
+                {String(pairId).replace("_", "/")}
+              </h3>
+              <FinVolumeChart
+                tickerId={pairId}
+                precision={tickPrecision.day1}
+                period={100}
+              />
             </div>
           </div>
         </div>
       );
     }
-
-    return charts;
-  };
+    setCharts(charts);
+  }, []);
 
   return (
     <main className="container py-5">
@@ -35,21 +50,27 @@ const Volumes = () => {
         Volumes
       </h1>
 
-      <div className="row">
-        <div className="col mb-3 mb-lg-4">
-          <VolumesInfoBar />
-        </div>
-      </div>
+      {/*<div className="row">*/}
+      {/*  <div className="col mb-3 mb-lg-4">*/}
+      {/*    <VolumesInfoBar />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
 
       <div className="row">
         <div className="col mb-3 mb-lg-4">
-          <input type="text" className="form-control bg-dark border-0" placeholder="Type to filter..." />
+          <input
+            id="filter"
+            name="filter"
+            type="text"
+            className="form-control bg-dark border-0 text-white"
+            placeholder="Type to filter..."
+            value={filter}
+            onChange={(event) => setFilter(event.target.value)}
+          />
         </div>
       </div>
 
-      <div className="row">
-        { volumeCharts() }
-      </div>
+      <div className="row">{ filteredCharts() }</div>
     </main>
   );
 };
