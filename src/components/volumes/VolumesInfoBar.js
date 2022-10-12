b161512dea0bf53c872734b651f56776e178ff60
Pedro Delgado <pdelgado@ocebot.com>
2022-10-12 15:39:55 +0200

    Total volume info bar correction.

--- src/components/volumes/VolumesInfoBar.js
+++ src/components/volumes/VolumesInfoBar.js
@@ -6,21 +6,28 @@ const VolumesInfoBar = () => {
   const [lastDayVol, setLastDayVolume] = useState(0);
 
   useEffect(() => {
+    const lastDayVolumePromises = [];
+
     for (const pairId in pairs) {
       const pair = new Pair(pairId);
-      pair.lastDayVolume().then((value) => {
-        setLastDayVolume((prev) => {
-          return prev + value;
-        });
-      });
+      lastDayVolumePromises.push(pair.lastDayVolume());
     }
+
+    Promise.all(lastDayVolumePromises).then((values) => {
+      const initialValue = 0;
+      setLastDayVolume(
+        values.reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          initialValue
+        )
+      );
+    });
   }, []);
 
   return (
     <div className="blue-gradient-bg rounded p-4 container-fluid fs">
       <div className="row">
         <div className="col text-white fs-5 fw-bold px-4 text-center text-md-start">
-
           <span className="d-block d-md-inline">
             <i className="bi bi-bar-chart-fill align-text-top me-3"></i>
             Accumulated volume
@@ -28,10 +35,7 @@ const VolumesInfoBar = () => {
           <span className="text-dark fw-light d-none d-md-inline mx-2">/</span>
           <span className="text-dark fw-light">yesterday</span>
 
-          <div className="float-md-end fw-light text-dark">
-            ${lastDayVol}
-          </div>
-
+          <div className="float-md-end fw-light text-dark">${lastDayVol}</div>
         </div>
       </div>
     </div>
