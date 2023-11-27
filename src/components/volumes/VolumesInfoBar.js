1a744788393c389d0ac9e84129ed7ebf8d309007
Juan Carlos Molina <jcmolina@ocebot.com>
2023-11-27 13:29:53 +0100

    Responsive and icon added

--- src/components/volumes/VolumesInfoBar.js
+++ src/components/volumes/VolumesInfoBar.js
@@ -49,6 +49,16 @@ const VolumesInfoBar = () => {
     });
   }, []);
 
+
+  const separatorV = (
+      <div className="vr p-0 text-white d-none d-lg-block"></div>
+  );
+  const separatorH = (
+      <hr
+          className="text-white my-4 d-block d-lg-none"
+          style={{ boxSizing: "border-box" }}
+      />
+  );
   return (
     <div className="blue-gradient-bg rounded p-4 container-fluid fs">
       <div className="row">
@@ -61,10 +71,11 @@ const VolumesInfoBar = () => {
           <span className="text-dark fw-light">yesterday</span>
           <div className="float-md-end fw-light text-dark">${lastDayVol}</div>
         </div>
-
+        {separatorV}
+        {separatorH}
         <div className="col text-white fs-5 fw-bold px-4 text-center text-md-start">
           <span className="d-block d-md-inline">
-            <i className="bi bi-bar-chart-fill align-text-top me-3"></i>
+            <i className="bi bi bi-cash-coin align-text-top me-3"></i>
             Accumulated Fees
           </span>
           <span className="text-dark fw-light d-none d-md-inline mx-2">/</span>
